99174dab2b6588ca64cb7429daa45b1dad1829dc
03112006mari <118907487+03112006mari@users.noreply.github.com>
2024-06-28 22:54:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "Obrigada por responder";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
